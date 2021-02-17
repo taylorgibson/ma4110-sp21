d6845ef68144858d1125c15200d9d9e4c95ee5b1
Taylor Gibson <taylorgibson@gmail.com>
2021-02-17 14:34:46 +0000

    Adding Lab 03

---
+++ labs/lab03/tests/q1_3_1.py
@@ -0,0 +1,1 @@
+test = {'name': 'q1_3_1', 'points': 1, 'suites': [{'cases': [{'code': '>>> sentence_length == 896\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
