0aabf1917935e10836e7926bdea28ed68281e50f
Taylor Gibson <taylorgibson@gmail.com>
2021-03-10 15:08:31 +0000

    Adding lab05

---
+++ labs/lab05/tests/q1_1.py
@@ -0,0 +1,1 @@
+test = {'name': 'q1_1', 'points': 1, 'suites': [{'cases': [{'code': '>>> number_cheese == 3\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
---
+++ labs/lab05/tests/q3_2.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q3_2',
+    'points': 1,
+    'suites': [   {   'cases': [   {'code': '>>> convenience_sample.num_columns\n11', 'hidden': False, 'locked': False},
+                                   {'code': '>>> convenience_sample.num_rows\n44', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
