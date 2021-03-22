4238bfbf913d5619991c6d1aeb9b194a1c0a111b
Taylor Gibson <taylorgibson@gmail.com>
2021-03-22 20:50:33 +0000

    adding lab07

---
+++ labs/lab07/tests/q3_10.py
@@ -0,0 +1,14 @@
+test = {   'name': 'q3_10',
+    'points': 1,
+    'suites': [   {   'cases': [   {   'code': '>>> np.random.seed(123);\n>>> simulate_and_test_statistic(change_in_death_rates, "Death Penalty", "Murder Rate")\n-1.3977272388840918',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': '>>> np.random.seed(123);\n'
+                                               '>>> np.isclose(round(simulate_and_test_statistic(change_in_death_rates, "Death Penalty", "Murder Rate"), 3) - (-1.398), 0)\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
