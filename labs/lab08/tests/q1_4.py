3f27d7574cbaed4fe5486afa3bf6699e93e2d622
Taylor Gibson <taylorgibson@gmail.com>
2021-04-28 13:49:21 +0000

    Adding Lab 08

---
+++ labs/lab08/tests/q1_4.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q1_4',
+    'points': 1,
+    'suites': [   {   'cases': [{'code': '>>> height_mean < 100\nTrue', 'hidden': False, 'locked': False}, {'code': '>>> height_sd < 100\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ labs/lab08/tests/q2_7.py
@@ -0,0 +1,1 @@
+test = {'name': 'q2_7', 'points': 1, 'suites': [{'cases': [{'code': '>>> q2_7 in [1,2,3,4]\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
